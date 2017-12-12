59b6cc1d02d3765f355fb89ea9f3d25cd193717c
dorgon.chang <dorgonman@hotmail.com>
2017-12-12 23:58:45 +0800

    Initial commit

---
+++ Source/HorizonFSMDemo/HorizonFSMDemoGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HorizonFSMDemoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HORIZONFSMDEMO_API AHorizonFSMDemoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
